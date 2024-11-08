ff78db9fb40ba9b196ed9b0969b6a2c110c7d503
Quentin <quent.jr@free.fr>
2024-11-08 16:53:15 +0100

    completion

---
+++ Apllication_web_Gestion/src/main/java/com/application_web_gestion/service/EnseignantService.java
@@ -0,0 +1,58 @@
+package com.application_web_gestion.service;
+
+import com.application_web_gestion.classe.Enseignant;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class EnseignantService {
+    private SessionFactory sessionFactory;
+
+    public EnseignantService() {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+    }
+
+    public void ajouterEnseignant(Enseignant enseignant) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(enseignant);
+        transaction.commit();
+        session.close();
+    }
+
+    public void modifierEnseignant(Enseignant enseignant) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(enseignant);
+        transaction.commit();
+        session.close();
+    }
+
+    public void supprimerEnseignant(int id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Enseignant enseignant = session.get(Enseignant.class, id);
+        if (enseignant != null) {
+            session.delete(enseignant);
+        }
+        transaction.commit();
+        session.close();
+    }
+
+    public Enseignant getEnseignant(int id) {
+        Session session = sessionFactory.openSession();
+        Enseignant enseignant = session.get(Enseignant.class, id);
+        session.close();
+        return enseignant;
+    }
+
+    public List<Enseignant> getAllEnseignant() {
+        Session session = sessionFactory.openSession();
+        List<Enseignant> enseignants = session.createQuery("from Enseignant", Enseignant.class).list();
+        session.close();
+        return enseignants;
+    }
+}
