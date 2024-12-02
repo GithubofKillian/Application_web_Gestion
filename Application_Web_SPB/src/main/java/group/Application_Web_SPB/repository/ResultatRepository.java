9380402037b24b4f345becea4461b43ec0ebe7b1
GithubofKillian <killian.jonneaux@gmail.com>
2024-12-02 23:17:54 +0100

    micro fix

--- Application_Web_SPB/src/main/java/group/Application_Web_SPB/repository/ResultatRepository.java
+++ Application_Web_SPB/src/main/java/group/Application_Web_SPB/repository/ResultatRepository.java
@@ -2,6 +2,10 @@ package group.Application_Web_SPB.repository;
 
 import group.Application_Web_SPB.entity.Resultat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ResultatRepository extends JpaRepository<Resultat, Long> {
+
+    // Recherche des résultats par l'ID de l'étudiant
+    List<Resultat> findByEtudiantId(Long etudiantId);
 }
